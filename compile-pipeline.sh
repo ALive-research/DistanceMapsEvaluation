c0d152d9d78c5b92c05f66eade5b889ca45ecd8c
Hector Martinez <hekthor.martinez@gmail.com>
2021-09-06 10:37:19 +0200

    Add a compiler script

---
+++ compile-pipeline.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#---COLORS---#
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+
+#---Variables---#
+build="build"
+modules="ITKMaurerDist VTKHausdorff vtk2vtp"
+
+
+echo "-------------------------------------------------------------------------"
+echo "          Compiling Modules for ITK/VTK Distances Test"
+echo "-------------------------------------------------------------------------"
+
+for module in $modules
+do	     
+    echo -e "[${YELLOW}'$module'${NC}] Compiling Module..."
+    cd $module
+    if [ -d $build ];then
+	rm -rf $build
+    fi
+    mkdir build
+    cd build
+    cmake ..
+    make
+    cd ../..
+    echo "-------------------------------------------------------------------------"
+done
