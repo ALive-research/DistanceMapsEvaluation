850169ed8bd6d0e4cae0bedae1accb5ce338c17a
Rafael Palomar <rafael.palomar@ous-research.no>
2022-06-13 11:43:21 +0200

    ENH: Add compression step on generate_data script

--- scripts/generate_dataset.sh
+++ scripts/generate_dataset.sh
@@ -19,7 +19,8 @@ ALL_DATASET=$(find $2 $3 -type f)
 declare -i livernr=0
 for i in ${LIVER_DATASET}; do
     echo "Processing ${i}"
-    ${LABELEXTRACT_OPERATOR} -l 1 -L 1 -i ${i} -o ${1}/liver_$(printf %03d $livernr).nrrd
+    ${LABELEXTRACT_OPERATOR} -l 1 -L 1 -i ${i} -o /tmp/label_tmp.nrrd
+    ${CONVERT_OPERATOR} -i /tmp/label_tmp.nrrd -d 0 -u -c -o ${1}/liver_$(printf %03d $livernr).nrrd
     livernr+=1
 done
 
@@ -27,7 +28,8 @@ done
 declare -i vesselnr=0
 for i in ${VESSELS_DATASET}; do
     echo "Processing ${i}"
-    ${LABELEXTRACT_OPERATOR} -l 1 -L 1 -i ${i} -o ${1}/vessels_$(printf %03d $vesselnr).nrrd
+    ${LABELEXTRACT_OPERATOR} -l 1 -L 1 -i ${i} -o /tmp/label_tmp.nrrd
+    ${CONVERT_OPERATOR} -i /tmp/label_tmp.nrrd -d 0 -u -c -o ${1}/vessels_$(printf %03d $vesselnr).nrrd
     vesselnr+=1
 done
 
@@ -35,6 +37,7 @@ done
 declare -i tumornr=0
 for i in ${ALL_DATASET}; do
     echo "Processing ${i}"
-    ${LABELEXTRACT_OPERATOR} -l 2 -L 1 -i ${i} -o ${1}/tumor_$(printf %03d $tumornr).nrrd
+    ${LABELEXTRACT_OPERATOR} -l 2 -L 1 -i ${i} -o /tmp/label_tmp.nrrd
+    ${CONVERT_OPERATOR} -i /tmp/label_tmp.nrrd -d 0 -u -c -o ${1}/tumor_$(printf %03d $tumornr).nrrd
     tumornr+=1
 done
